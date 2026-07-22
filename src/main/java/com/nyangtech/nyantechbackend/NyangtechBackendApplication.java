fd3c90590a43527d7cf7a567e9b15a96fb732bed
h-nxux <arthur_02@naver.com>
2026-07-22 09:56:26 +0900

    chore: Spring Boot 프로젝트 초기 세팅

---
+++ src/main/java/com/nyangtech/nyantechbackend/NyangtechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nyangtech.nyantechbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyangtechBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NyangtechBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nyangtech/nyantechbackend/NyangtechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyangtech.nyantechbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NyangtechBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
